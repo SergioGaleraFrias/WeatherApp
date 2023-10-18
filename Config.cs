b46253b61d937e1de5a0781f004faa94dadb4ec2
Sergio <sergio.galerafr@elorrieta-errekamari.com>
2023-10-18 08:23:38 +0200

    Agregar archivos de proyecto.

---
+++ Config.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp
+{
+    public class Config
+    {
+        //Pongo mi clave de geoapify
+        public static string GeoApifyKey = "8a836cae59814458875bcc2697a1de3c";
+    }
+}
---
+++ EndPoints.cs
@@ -0,0 +1,21 @@
+﻿using static System.Net.WebRequestMethods;
+
+namespace WeatherApp
+{
+    public class EndPoints
+    {
+        public static string GetGeoApifyEndPoint(string city)
+        {
+            //return $"https://api.geoapify.com/v1/geocode/search?text=Bilbao&format=json&apiKey=8a836cae59814458875bcc2697a1de3c";
+            return $"https://api.geoapify.com/v1/geocode/search?text={city}&format=json&apiKey={Config.GeoApifyKey}";
+        }
+
+        public static string GetWeatherEndPoint(float lat, float lon)
+        {
+            //var url = $"https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relativehumidity_2m&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=Europe%2FLondon";
+            var url = $"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,relativehumidity_2m&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=Europe%2FLondon";
+            return url ;
+        }
+
+    }
+}
